73ac74567b3b383bc03859de3f19df2e04333d8c
Matt Razza <504088+mrazza@users.noreply.github.com>
2026-01-18 00:36:22 -0500

    fix: show artist (previously missing) in NowPlaying table

--- smoc/Ui/PlayerView.cs
+++ smoc/Ui/PlayerView.cs
@@ -30,7 +30,7 @@ public sealed class PlayerView : View
             Y = Pos.Center(),
             Text = "queue is empty"
         };
-        songTable = new SongTable(SongTableColumns.Album | SongTableColumns.Song | SongTableColumns.Length | SongTableColumns.Year)
+        songTable = new SongTable(SongTableColumns.Artist | SongTableColumns.Album | SongTableColumns.Song | SongTableColumns.Length | SongTableColumns.Year)
         {
             Width = Dim.Fill(),
             Height = Dim.Fill()
