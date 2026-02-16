1175ab97d896de54a5a6df4ba303b062248ccfdb
Matt Razza <504088+mrazza@users.noreply.github.com>
2026-02-15 19:21:48 -0500

    docs: update readme to contain documentation on configuration settings

--- smoc/Configuration/ListenHistoryConfig.cs
+++ smoc/Configuration/ListenHistoryConfig.cs
@@ -15,20 +15,17 @@ public static class ListenHistoryConfig {
   /// Gets or sets whether listen history tracking is enabled.
   /// </summary>
   [ConfigurationProperty(Scope = typeof(SettingsScope))]
-  [property: JsonPropertyName("enabled")]
   public static bool Enabled { get; set; } = true;
 
   /// <summary>
   /// Gets or sets the minimum position in seconds for a song to be considered listened to.
   /// </summary>
   [ConfigurationProperty(Scope = typeof(SettingsScope))]
-  [property: JsonPropertyName("minimumPositionSeconds")]
   public static int MinimumPositionSeconds { get; set; } = 30;
 
   /// <summary>
   /// Gets or sets the minimum fraction of a song for it to be considered listened to.
   /// </summary>
   [ConfigurationProperty(Scope = typeof(SettingsScope))]
-  [property: JsonPropertyName("minimumFraction")]
   public static double MinimumFraction { get; set; } = 0.5;
 }
